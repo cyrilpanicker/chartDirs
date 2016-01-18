bd145b7e2d58c0e1fb2871716099c20937e383cf
cyrilpanicker <cyrilpanicker@gmail.com>
2016-01-18 22:55:26 +0530

    added changes

--- scripts-src/chartDirs.ts
+++ scripts-src/chartDirs.ts
@@ -8,28 +8,26 @@ interface IBarChartScope extends ng.IScope{
 }
 
 interface DrawingConfig{
+    svgElement:d3.Selection<number>;
     data:{}[];
     keyProp:string;
     valueProp:string[];
+    scale:d3.scale.Linear<number,number>;
 }
 
 angular.module('chartDirs',[])
 .directive('barChart',() => {
     
-    var updateSet:d3.selection.Update<{}>,
-        svgElement:d3.Selection<number>,
-        scale:d3.scale.Linear<number,number>;
-    
     var draw = (config:DrawingConfig) => {
-        updateSet = svgElement.selectAll('rect')
+        var updateSet = config.svgElement.selectAll('rect')
             .data(config.data,datum => datum[config.keyProp]);
         updateSet.enter().append('rect');
         updateSet
             .transition().duration(500)
             .delay((datum,index) => index*50)
             .attr('x',(datum,index) => index*100)
-            .attr('y',datum => 500-(scale(datum[config.valueProp[0]])*100))
-            .attr('height',datum => scale(datum[config.valueProp[0]])*100)
+            .attr('y',datum => 500-(config.scale(datum[config.valueProp[0]])*100))
+            .attr('height',datum => config.scale(datum[config.valueProp[0]])*100)
             .attr('width',50)
             .attr('fill','blue');
     };
@@ -42,16 +40,18 @@ angular.module('chartDirs',[])
             maxValue:'@'
         },
         link:(scope:IBarChartScope, element:ng.IAugmentedJQuery, attributes) => {
-            svgElement = d3.select(element[0]).append('svg')
+            var svgElement = d3.select(element[0]).append('svg')
                 .attr('width',1000)
                 .attr('height',500);
-            scale = d3.scale.linear().domain([0,parseFloat(scope.maxValue)]).range([0,5]);
+            var scale = d3.scale.linear().domain([0,parseFloat(scope.maxValue)]).range([0,5]);
             scope.$watch('data',(newValue,oldValue) => {
                 if(scope.data){
                     draw({
+                        svgElement:svgElement,
                         data:scope.data,
                         keyProp:scope.keyProp,
-                        valueProp:scope.valueProp
+                        valueProp:scope.valueProp,
+                        scale:scale
                     });
                 }
             },true);
--- scripts/chartDirs.js
+++ scripts/chartDirs.js
@@ -1,17 +1,16 @@
 // /// <reference path="typings/tsd.d.ts" />
 angular.module('chartDirs', [])
     .directive('barChart', function () {
-    var updateSet, svgElement, scale;
     var draw = function (config) {
-        updateSet = svgElement.selectAll('rect')
+        var updateSet = config.svgElement.selectAll('rect')
             .data(config.data, function (datum) { return datum[config.keyProp]; });
         updateSet.enter().append('rect');
         updateSet
             .transition().duration(500)
             .delay(function (datum, index) { return index * 50; })
             .attr('x', function (datum, index) { return index * 100; })
-            .attr('y', function (datum) { return 500 - (scale(datum[config.valueProp[0]]) * 100); })
-            .attr('height', function (datum) { return scale(datum[config.valueProp[0]]) * 100; })
+            .attr('y', function (datum) { return 500 - (config.scale(datum[config.valueProp[0]]) * 100); })
+            .attr('height', function (datum) { return config.scale(datum[config.valueProp[0]]) * 100; })
             .attr('width', 50)
             .attr('fill', 'blue');
     };
@@ -23,16 +22,18 @@ angular.module('chartDirs', [])
             maxValue: '@'
         },
         link: function (scope, element, attributes) {
-            svgElement = d3.select(element[0]).append('svg')
+            var svgElement = d3.select(element[0]).append('svg')
                 .attr('width', 1000)
                 .attr('height', 500);
-            scale = d3.scale.linear().domain([0, parseFloat(scope.maxValue)]).range([0, 5]);
+            var scale = d3.scale.linear().domain([0, parseFloat(scope.maxValue)]).range([0, 5]);
             scope.$watch('data', function (newValue, oldValue) {
                 if (scope.data) {
                     draw({
+                        svgElement: svgElement,
                         data: scope.data,
                         keyProp: scope.keyProp,
-                        valueProp: scope.valueProp
+                        valueProp: scope.valueProp,
+                        scale: scale
                     });
                 }
             }, true);
